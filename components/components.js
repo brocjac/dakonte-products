5d19fd3ed4ff572c909e0b177b0ee9f15fbf71ec
brocjac <brockwell2001@gmail.com>
2023-11-01 11:37:05 -0500

    back to reality

--- components/components.js
+++ components/components.js
@@ -4,55 +4,55 @@ class Header extends HTMLElement {
         this.innerHTML = `
         <header>
             <a href="index.html"><img class="logo-img-text" src="images/images/DPG-Corp-Family-Logo-TIE-GMC-EP-transparent.jpg" alt="Dakonte Product Group, TIE office mates, Great Menu Covers, and Easy Partition Logo. " loading="lazy"></a>
-<!--            <div class="nav" aria-label="main navigation">-->
-<!--                <ul>-->
-<!--                    <li>-->
-<!--                        <div class="hover">-->
-<!--                            <a onclick="show_hide_1()" href="#" class="menuItem hover-menu list-link-1">Products</a>-->
-<!--                            <div class="list list-1" style="display: none;">-->
-<!--                                <ul>-->
-<!--                                    <li><a class="list-links" href="https://tieofficemates.com/shop-ticket-holders/heavy-duty-stitched-job-ticket-holders/">Job Ticket Holders</a></li>-->
-<!--                                    <li><a class="list-links" href="https://tieofficemates.com/vinyl-document-sleeves/waterproof-press-lock-seal/">Waterproof Holders</a></li>-->
-<!--                                    <li><a class="list-links" href="https://tieofficemates.com/color-coding-options/">Color Coded Products</a></li>-->
-<!--                                    <li><a class="list-links" href="https://greatmenucovers.com/cafe-menu-covers-vinyl-trim">Café Menu Covers</a></li>-->
-<!--                                    <li><a class="list-links" href="https://greatmenucovers.com/clear-plastic-menu-sleeve-pockets/">Clear Menu Covers</a></li>-->
-<!--                                    <li><a class="list-links" href="https://greatmenucovers.com/add-a-menu-page-inserts/">Add-A-Page</a></li>-->
-<!--                                    <li><a class="list-links" href="https://tieofficemates.com/auto-repair-order-holders/">Service Order Holders</a></li>-->
-<!--                                    <li><a class="list-links" href="https://tieofficemates.com/easy-sign-holders/">Sign Holders</a></li>-->
-<!--                                    <li><a class="list-links" href="https://tieofficemates.com/vinyl-tag-holders/">Tag Holders</a></li>-->
-<!--                                </ul>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                    </li>-->
-<!--                    <li>-->
-<!--                        <div class="hover">-->
-<!--                            <a onclick="show_hide_2()" href="#" class="menuItem hover-menu list-link-2">Industry</a>-->
-<!--                            <div class="list list-2" style="display: none;">-->
-<!--                                <ul>-->
-<!--                                    <li><a class="list-links" href="#">Auto Repair</a></li>-->
-<!--                                    <li><a class="list-links" href="#">Construction</a></li>-->
-<!--                                    <li><a class="list-links" href="#">Education</a></li>-->
-<!--                                    <li><a class="list-links" href="#">Facility Manager</a></li>-->
-<!--                                    <li><a class="list-links" href="#">Government</a></li>-->
-<!--                                    <li><a class="list-links" href="#">Healthcare</a></li>-->
-<!--                                    <li><a class="list-links" href="#">Manufacturer</a></li>-->
-<!--                                    <li><a class="list-links" href="#">Office</a></li>-->
-<!--                                    <li><a class="list-links" href="#">Retail</a></li>-->
-<!--                                </ul>-->
-<!--                                <div>-->
-<!--                                    <img src="images/about-us/sneeze-guards-banner-725x250-usa.jpg" alt="">-->
-<!--                                    <p>this is an image</p>-->
-<!--                                </div>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                    </li>-->
-<!--                    <li><a href="brands.html" class="menuItem hover-menu">Brands</a></li>-->
-<!--                </ul>-->
-<!--            </div>-->
-<!--            <button class="hamburger">-->
-<!--                <i class="menuIcon material-icons">menu</i>-->
-<!--                <i class="closeIcon material-icons" style="display: none">close</i>-->
-<!--            </button>-->
+            <div class="nav" aria-label="main navigation">
+                <ul>
+                    <li>
+                        <div class="hover">
+                            <a onclick="show_hide_1()" href="#" class="menuItem hover-menu list-link-1">Products</a>
+                            <div class="list list-1" style="display: none;">
+                                <ul>
+                                    <li><a class="list-links" href="https://tieofficemates.com/shop-ticket-holders/heavy-duty-stitched-job-ticket-holders/">Job Ticket Holders</a></li>
+                                    <li><a class="list-links" href="https://tieofficemates.com/vinyl-document-sleeves/waterproof-press-lock-seal/">Waterproof Holders</a></li>
+                                    <li><a class="list-links" href="https://tieofficemates.com/color-coding-options/">Color Coded Products</a></li>
+                                    <li><a class="list-links" href="https://greatmenucovers.com/cafe-menu-covers-vinyl-trim">Café Menu Covers</a></li>
+                                    <li><a class="list-links" href="https://greatmenucovers.com/clear-plastic-menu-sleeve-pockets/">Clear Menu Covers</a></li>
+                                    <li><a class="list-links" href="https://greatmenucovers.com/add-a-menu-page-inserts/">Add-A-Page</a></li>
+                                    <li><a class="list-links" href="https://tieofficemates.com/auto-repair-order-holders/">Service Order Holders</a></li>
+                                    <li><a class="list-links" href="https://tieofficemates.com/easy-sign-holders/">Sign Holders</a></li>
+                                    <li><a class="list-links" href="https://tieofficemates.com/vinyl-tag-holders/">Tag Holders</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="hover">
+                            <a onclick="show_hide_2()" href="#" class="menuItem hover-menu list-link-2">Industry</a>
+                            <div class="list list-2" style="display: none;">
+                                <ul>
+                                    <li><a class="list-links" href="#">Auto Repair</a></li>
+                                    <li><a class="list-links" href="#">Construction</a></li>
+                                    <li><a class="list-links" href="#">Education</a></li>
+                                    <li><a class="list-links" href="#">Facility Manager</a></li>
+                                    <li><a class="list-links" href="#">Government</a></li>
+                                    <li><a class="list-links" href="#">Healthcare</a></li>
+                                    <li><a class="list-links" href="#">Manufacturer</a></li>
+                                    <li><a class="list-links" href="#">Office</a></li>
+                                    <li><a class="list-links" href="#">Retail</a></li>
+                                </ul>
+                                <div>
+                                    <img src="images/about-us/sneeze-guards-banner-725x250-usa.jpg" alt="">
+                                    <p>this is an image</p>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li><a href="brands.html" class="menuItem hover-menu">Brands</a></li>
+                </ul>
+            </div>
+            <button class="hamburger">
+                <i class="menuIcon material-icons">menu</i>
+                <i class="closeIcon material-icons" style="display: none">close</i>
+            </button>
         </header>
         `;
     }
@@ -64,7 +64,7 @@ class Footer extends HTMLElement {
             <footer>
                 <img class="logo-img-footer" src="../images/icons/about-dgp.png" alt="Dakonte Product Group logo. " loading="lazy">
                 <div aria-description="about us and privacy pages">
-<!--                    <p><a href="about-dpg.html">About DGP</a> | <a href="privacy.html">Privacy</a></p>-->
+                    <p><a href="about-dpg.html">About DGP</a> | <a href="privacy.html">Privacy</a></p>
                     <p class="copywrite">© Dakonte Product Group, Inc 2023. All Rights Reserved.</p>
                 </div>
                 <p class="trademark">The brands listed above are trademarks of Dakonte Product Group.</p>
